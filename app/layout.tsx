389b5951a990ff38467ac81bd38b650deb540f48
Somesh Chavhan <someshchavhan904@gmail.com>
2024-01-01 19:56:01 +0530

    favicon added

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,20 +1,24 @@
-import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
-import './globals.css'
-import Navbar from '@/components/Navbar'
-import Navigation from '@/components/Navigation'
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+import Navbar from "@/components/Navbar";
+import Navigation from "@/components/Navigation";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: 'Webchain dev',
-  description: 'This is my portfolio',
-}
+  title: "Somesh Chavhan",
+  description: "Somesh Chavhan's portfolio",
+  icons: {
+    icon: ["/favicon.ico"],
+    apple: ["apple-touch-icon.png"],
+  },
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
@@ -22,7 +26,7 @@ export default function RootLayout({
         <Navbar />
         {children}
         <Navigation />
-        </body>
+      </body>
     </html>
-  )
+  );
 }
